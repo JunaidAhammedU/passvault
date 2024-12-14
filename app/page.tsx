309a9cc67112b4ba5b1da7996922bd02ede003fa
JunaidAhammedU <junaidahammed15@gmail.com>
2024-12-14 09:55:06 +0530

    header

--- app/page.tsx
+++ app/page.tsx
@@ -14,57 +14,61 @@ import {
   BsTwitch,
   BsTwitterX,
 } from "react-icons/bs";
+import Navbar from "@/components/ui/Header";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="min-h-screen flex items-center justify-center px-4 sm:px-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 sm:p-5 md:px-6 xl:gap-32">
-        <div className="flex flex-col text-start justify-center items-center relative">
-          <div className="text-start mt-12 lg:mt-24">
-            <h1
-              className={`${mona.className} text-4xl md:text-7xl lg:text-7xl text-black font-extrabold leading-tight`}
-            >
-              Safe place <br />
-              for all your <br /> passwords
-            </h1>
-          </div>
-          <div className="mt-8 flex flex-col sm:flex-row sm:justify-center gap-4">
-            <Button className="bg-black text-white text-lg py-3 px-6 hover:bg-gray-800 transition rounded-sm">
-              Generate Password*
-            </Button>
-            <Button className="bg-white text-black text-lg py-3 px-6 hover:bg-gray-100 transition rounded-sm">
-              Manage Passwords
-            </Button>
-          </div>
-          <div className="mt-4 md:mt-12">
-            <p className="text-sm mt-4 text-gray-700">
-              Over 10,000 business-friendly passwords and counting...
-            </p>
-            <div className="flex justify-center gap-4 mt-4 text-gray-700">
-              <BsGoogle />
-              <BsFacebook />
-              <BsLinkedin />
-              <BsThreadsFill />
-              <BsTwitch />
-              <BsTwitterX />
+    <>
+      <Navbar />
+      <div className="min-h-screen flex items-center justify-center px-4 sm:px-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 sm:p-5 md:px-6 xl:gap-32">
+          <div className="flex flex-col text-start justify-center items-center relative">
+            <div className="text-start mt-12 lg:mt-24">
+              <h1
+                className={`${mona.className} text-4xl md:text-7xl lg:text-7xl text-black font-extrabold leading-tight`}
+              >
+                Safe place <br />
+                for all your <br /> passwords
+              </h1>
+            </div>
+            <div className="mt-8 flex flex-col sm:flex-row sm:justify-center gap-4">
+              <Button className="bg-black text-white text-lg py-3 px-6 hover:bg-gray-800 transition rounded-sm">
+                Generate Password*
+              </Button>
+              <Button className="bg-white text-black text-lg py-3 px-6 hover:bg-gray-100 transition rounded-sm">
+                Manage Passwords
+              </Button>
+            </div>
+            <div className="mt-4 md:mt-12">
+              <p className="text-sm mt-4 text-gray-700">
+                Over 10,000 business-friendly passwords and counting...
+              </p>
+              <div className="flex justify-center gap-4 mt-4 text-gray-700">
+                <BsGoogle />
+                <BsFacebook />
+                <BsLinkedin />
+                <BsThreadsFill />
+                <BsTwitch />
+                <BsTwitterX />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex justify-center items-center p-10 md:p-2">
-          <Image
-            src="/landing2.png"
-            alt="Password management illustration"
-            className="object-contain rounded-md  "
-            width={500}
-            height={500}
-            priority
-            sizes="(max-width: 768px) 100vw, 50vw"
-          />
+          <div className="flex justify-center items-center p-10 md:p-2">
+            <Image
+              src="/landing2.png"
+              alt="Password management illustration"
+              className="object-contain rounded-md  "
+              width={500}
+              height={500}
+              priority
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ components/ui/Header.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-wrap items-center justify-between px-6 py-4 z-50 fixed w-full bg-white">
+      <div className="flex items-center">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          className="h-6 w-6 object-cover"
+          width={24}
+          height={24}
+          priority
+        />
+        <span className="pl-2 mb-2 text-xl font-bold">passvault</span>
+      </div>
+
+      <div className="hidden md:flex items-center space-x-6">
+        <a className="text-gray-700 hover:text-black">Product</a>
+        <a className="text-gray-700 hover:text-black">Features</a>
+        <a className="text-gray-700 hover:text-black">Marketplace</a>
+        <a className="text-gray-700 hover:text-black">Company</a>
+      </div>
+
+      <div className="hidden md:flex">
+        <a className="text-black flex items-center">
+          Log in <span className="ml-1">→</span>
+        </a>
+      </div>
+
+      <div className="md:hidden flex items-center">
+        <button className="focus:outline-none">
+          <svg
+            className="w-6 h-6 text-gray-700"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
