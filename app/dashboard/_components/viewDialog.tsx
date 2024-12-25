81e56de4dcef52390a2dd77d6b26de68d73aaa8d
JunaidAhammedU <junaidahammed15@gmail.com>
2024-12-25 10:38:45 +0530

    refactor viewDialog component to enhance UI and functionality; implement password visibility toggle and clipboard copy feature

--- app/dashboard/_components/viewDialog.tsx
+++ app/dashboard/_components/viewDialog.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import {
   AlertDialog,
   AlertDialogContent,
@@ -7,29 +9,52 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { FaRegCopy } from "react-icons/fa";
 
 function ViewDialoge({ openOutputDialog, closeOutputDialog, name, url }: any) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(url);
+  };
+
   return (
     <AlertDialog open={openOutputDialog}>
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle className="text-center text-lg">
-            {" "}
-            Here is your result.
+            <div className="text-center text-xl md:text-2xl">{name}</div>
           </AlertDialogTitle>
         </AlertDialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
+          <div className="grid grid-cols-4 items-center gap-4 ">
             <label htmlFor="name" className="text-right">
               Username
             </label>
-            <Input id="name" value={name} className="col-span-3" />
+            <div className="border border-gray-300 text-black col-span-3 w-full rounded-md h-10 items-center flex px-4 mt-4">
+              <h1>{name}</h1>
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
+          <div className="grid grid-cols-4 items-center gap-4 relative">
             <label htmlFor="username" className="text-right">
               Password
             </label>
-            <Input id="username" value={url} className="col-span-3" />
+            <div
+              className="border border-gray-300 text-black col-span-3 rounded-md h-10 items-center flex px-4 relative mt-4"
+              onClick={() => setIsClicked(true)}
+            >
+              <h1
+                className={`${
+                  !isClicked ? `absolute bottom-1` : `items-center`
+                }`}
+              >
+                {isClicked ? url : "********"}
+              </h1>
+              <FaRegCopy
+                className="absolute right-3 text-gray-600 cursor-pointer hover:text-gray-900"
+                onClick={handleCopy}
+              />
+            </div>
           </div>
         </div>
         <Button onClick={() => closeOutputDialog(false)}>Close</Button>
