8d849ef39565b3ac7a8a3400bc09c6665d0268e3
JunaidAhammedU <junaidahammed15@gmail.com>
2024-12-30 09:51:45 +0530

    feat: add ShowCards component to AllItems page

--- app/dashboard/manage-password/all-items/page.tsx
+++ app/dashboard/manage-password/all-items/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AllItemsLIst from "../../_components/all-items-list";
+import ShowCards from "../../_components/showCards";
 
 export default function AllItems() {
   return (
     <>
       <div className="px-3 py-8 md:p-20 mt-[70px] mb-10 bg-[#F8F8F8] h-full">
+        <ShowCards />
         <AllItemsLIst />
       </div>
     </>
