7e81c823161678de5ceebd8395f4a8334932a6f0
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-06 10:27:40 +0530

    fav fetch based on user email

--- app/api/favorite-pwd/route.ts
+++ app/api/favorite-pwd/route.ts
@@ -4,10 +4,19 @@ import connectToDatabase from "@/config/db";
 
 export async function GET(req: Request) {
   try {
+    const url = new URL(req.url);
+    const userEmail = url.searchParams.get("email");
+
+    if (!userEmail) {
+      return NextResponse.json(
+        { error: "Email parameter is required" },
+        { status: 400 }
+      );
+    }
     await connectToDatabase();
-    const pass = await Passwords.find({
-      isFavorite: true,
-    }).sort({ createdAt: -1 });
+    const pass = await Passwords.find({ userEmail, isFavorite: true }).sort({
+      createdAt: -1,
+    });
     return NextResponse.json(pass, { status: 200 });
   } catch (error: any) {
     return NextResponse.json(
