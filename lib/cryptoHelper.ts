a7d0fc3b8306cfa2f4f0523206ddff4695e41aa9
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-10 11:12:32 +0530

    feat: add encryption and decryption functions in cryptoHelper

--- lib/cryptoHelper.ts
+++ lib/cryptoHelper.ts
@@ -3,11 +3,13 @@ import CryptoJS from "crypto-js";
 const SECRET_KEY: string =
   process.env.NEXT_PUBLIC_SECRET_KEY || "your-secure-key";
 
+// encrypt pass
 export const encryptData = (data: string): string => {
   const ciphertext = CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
   return ciphertext;
 };
 
+// decrypt pass
 export const decryptData = (ciphertext: string): string => {
   const bytes = CryptoJS.AES.decrypt(ciphertext, SECRET_KEY);
   const originalText = bytes.toString(CryptoJS.enc.Utf8);
