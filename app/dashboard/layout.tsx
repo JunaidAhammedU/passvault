6345c3a9027b24964eb674f7cea83ee59cb98b39
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-11 10:20:59 +0530

    fix: adjust margin-top in DashboardLayout for better spacing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./_components/Footer";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mt10">
+    <div className="md:mt-24">
       <Navbar />
       <div>{children}</div>
       <Footer />
